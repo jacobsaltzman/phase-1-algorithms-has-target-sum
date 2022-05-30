b475a64047afc27dc3dad4924b5568abe688ffa4
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-05-30 12:18:24 -0700

    commit tree c8abec00afd42d7dd07052f0e11887201cfd05c6

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+    for (let i = 0; i < array.length; i++){
+      for(let j =i+1; j<array.length; j++){
+        if(array[i] + array[j] == target){
+          return true
+        }
+      }
+    }
+    return false
 }
-
 /* 
   Write the Big O time complexity of your function here
+  O(n) because it iterates through each item in the array
+  
 */
 
 /* 
   Add your pseudocode here
+  pop number off
+  iterate each num in comparison
+  returning true if target is equals
 */
 
 /*
   Add written explanation of your solution here
+  take the last number off and add it to each other item in the array to see if they add up to target.
+  repeat in a loop until no items left
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +42,14 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 2, 3, 3], 4));
 }
 
 module.exports = hasTargetSum;
